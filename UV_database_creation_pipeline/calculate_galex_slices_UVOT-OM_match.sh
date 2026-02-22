a78dfb0273a518a14a622d7643d54b726c101101
JulienValteau <jln.valteau@club-internet.fr>
2026-02-22 23:11:01 +0100

    Added match with Regalade catalogue, updated calibration results for UVOT-OM, added match script for Galex slices and UVOT-OM galaxies sources

---
+++ UV_database_creation_pipeline/calculate_galex_slices_UVOT-OM_match.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/bash
+j=0
+datapath="../../../Data"
+for f in $( ls $datapath/Galex/GUVCat_*.csv.gz); do
+
+	echo "Extracting the required columns for " $f
+	stilts tpipe ifmt=csv cmd='keepcols "objid ra dec fuv_flux \
+	fuv_fluxerr nuv_flux nuv_fluxerr nuv_poserr"' \
+	omode=out ofmt=csv in=$f out=$datapath/Galex/temp.csv
+
+	echo "Performing match with UVOT-OM catalogue for " $f
+	COUNT="$(stilts tmatch2 ifmt1=csv ifmt2=csv in1=$datapath/Galex/temp.csv \
+	in2=$datapath/SOURCE_complete_galaxies.csv matcher=skyerr \
+	values1="ra dec 3*nuv_poserr" values2="RA DEC 3*POSERR" \
+	join=1and2 find=best params=1 omode=count progress=none \
+    | cut -d ":" -f 3)"
+
+    echo "Number of matches : ${COUNT}"
+
+    if [ "${COUNT}" -gt 0 ]; then
+	    stilts tmatch2 ifmt1=csv ifmt2=csv in1=$datapath/Galex/temp.csv \
+	    in2=$datapath/SOURCE_complete_galaxies.csv matcher=skyerr \
+	    values1="ra dec 3*nuv_poserr" values2="RA DEC 3*POSERR" \
+        ocmd="addcol angDist skyDistanceDegrees(ra_1,dec_1,RA_2,DEC_2)*3600" \
+        ocmd='keepcols "objid ra_1 dec_1 fuv_flux fuv_fluxerr nuv_flux \
+	    nuv_fluxerr nuv_poserr SRCNUM_1 SRCNUM_2 RA_2 DEC_2 POSERR COUNT angDist"' \
+	    join=1and2 find=best ofmt=csv params=1 out=$datapath/Galex/temp_match.csv \
+        progress=none
+
+	    if [ $j -gt 0 ]; then
+      		echo "Concatenating matched slices"
+		    stilts tcat ifmt=csv in="$datapath/Galex/match_Galex_UVOT_OM.csv \
+            $datapath/Galex/temp_match.csv" \
+            icmd="replacecol SRCNUM_1 toInteger(SRCNUM_1)" \
+            icmd="replacecol SRCNUM_2 toInteger(SRCNUM_2)" \
+            out=$datapath/Galex/temp.csv
+		    
+		    mv $datapath/Galex/temp.csv $datapath/Galex/match_Galex_UVOT_OM.csv
+	    else
+		    mv $datapath/Galex/temp_match.csv $datapath/Galex/match_Galex_UVOT_OM.csv
+	    fi
+	    let j=j+1
+
+    else
+        echo "No match for " $f
+    fi
+    
+    echo ""
+done
