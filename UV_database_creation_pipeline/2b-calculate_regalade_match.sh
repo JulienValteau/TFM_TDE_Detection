f1974dbb61a588709078570a284afa13fe35aa3e
JulienValteau <jln.valteau@club-internet.fr>
2026-03-19 23:56:59 +0100

    Added corrections according to meeting 17th of March

--- UV_database_creation_pipeline/2b-calculate_regalade_match.sh
+++ UV_database_creation_pipeline/2b-calculate_regalade_match.sh
@@ -11,7 +11,7 @@ join=1and2 find=best ofmt=fits params=1 out=$datapath/XMM-Newton/Match_regalade/
 echo "Computing random match values"
 stilts tmatch2 ifmt1=fits ifmt2=fits in1=$datapath/XMM-Newton/SUSS6_ra_dec_per_src.fits \
 in2=$datapath/Regalade/regalade_ra_dec.fits matcher=skyerr \
-values1="RA+1.0/60.0 DEC 3*POSERR" values2="ra dec 0" \
+values1="RA+0.016667 DEC 3*POSERR" values2="ra dec 0" \
 join=1and2 find=best params=1 omode=count > $datapath/XMM-Newton/Match_regalade/count_random_match.txt
 
 datapath="../../../Data"
